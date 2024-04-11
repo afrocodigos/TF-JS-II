f6e613e0262b938d0ab948b2cbb164133578b8a6
Andreza Cristina <andrezadesenvolvedoraweb@gmail.com>
2024-04-10 21:57:46 -0300

    Update index.js
    atividade realizado

--- aula02/index.js
+++ aula02/index.js
@@ -29,3 +29,33 @@ contador++; // some mais um ao contador
 // do {
 //   numero = prompt("Digite um número");
 // } while (numero !== "10");
+
+
+
+
+// Jogo da adivinhação: desenvolva um jogo que permita que o usuário 
+// tente adivinhar um número inteiro aleatório entre 1 e 100. Utilize um loop do/while, 
+// o programa deve fornecer dicas como "maior" ou "menor" após cada tentativa, 
+// ao acertar deve informar o número adivinhado e número de tentativas realizadas para descobrir.
+
+const prompt = require('prompt-sync')();
+
+let numeroAleatorio =  Math.floor(Math.random() *100) +1;
+let tentativas = 0;
+let palpite 
+console.log(numeroAleatorio);
+
+do {
+    palpite = parseInt(prompt('Tente adivinhar o número secreto entre 1 e 100: '));
+    
+    if(palpite < numeroAleatorio){
+
+        console.log("Tente novamente. O número secreto é maior. ") ;
+    
+    } else if (palpite > numeroAleatorio){
+        console.log("Tente novamente. O número secreto é menor." );
+    }
+    tentativas++
+
+}while (palpite !== numeroAleatorio);
+console.log(`Parabéns! Você acertou o número secreto em ${tentativas} tentativas`)
